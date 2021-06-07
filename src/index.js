129dfebbeba83a7d3a4ce1acc8602e9b82c4a390
m-kawahigashi <50309689+m-kawahigashi@users.noreply.github.com>
2021-06-07 23:03:06 +0900

    Create click event(addButton)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css";
+
+//追加ボタン押下時のクリックイベント
+const onClickAdd = () => {
+  //入力されたTODOを取得
+  const inputText = document.getElementById("add-text").value;
+  //入力されたTODOを削除
+  document.getElementById("add-text").value = "";
+  alert(inputText);
+};
+
+document
+  .getElementById("add-btn")
+  .addEventListener("click", () => onClickAdd());
