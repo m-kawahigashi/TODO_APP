8c1b41d3d7def3021f28224eac9f40bfacf583ba
m-kawahigashi <50309689+m-kawahigashi@users.noreply.github.com>
2021-06-13 18:10:53 +0900

    UPDATE　add to buttonTag 追加、削除ボタンを追加

--- src/index.js
+++ src/index.js
@@ -15,8 +15,20 @@ const onClickAdd = () => {
   const liTag = document.createElement("li");
   liTag.innerText = inputText;
 
-  //divタグの子要素としてliタグを設定
+  //完了ボタンを生成
+  const completeButtonTag = document.createElement("button");
+  completeButtonTag.innerText = "完了";
+  //console.log(completeButtonTag);
+
+  //削除ボタンを生成
+  const deleteButtonTag = document.createElement("button");
+  deleteButtonTag.innerText = "削除";
+  //console.log(deleteButtonTag);
+
+  //入力されたTODO・ボタンタグを追加する
   divTag.appendChild(liTag);
+  divTag.appendChild(completeButtonTag);
+  divTag.appendChild(deleteButtonTag);
 
   //ulタグの子要素としてdivタグを設定
   document.getElementById("incomplete-list").appendChild(divTag);
