188e0a62b753bc4e5b91ebe502c616be2499e34c
m-kawahigashi <50309689+m-kawahigashi@users.noreply.github.com>
2021-06-13 18:44:29 +0900

    update delete event 削除ボタン押下後のイベントを追加

--- src/index.js
+++ src/index.js
@@ -24,6 +24,13 @@ const onClickAdd = () => {
   const deleteButtonTag = document.createElement("button");
   deleteButtonTag.innerText = "削除";
   //console.log(deleteButtonTag);
+  //削除ボタン押下後、押下されたTODOを削除する
+  deleteButtonTag.addEventListener("click", () => {
+    //ボタン押下されたTODO(divタグ)を取得
+    const deleteTarget = deleteButtonTag.parentNode;
+    //TODO(親要素)を削除
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
 
   //入力されたTODO・ボタンタグを追加する
   divTag.appendChild(liTag);
@@ -34,6 +41,7 @@ const onClickAdd = () => {
   document.getElementById("incomplete-list").appendChild(divTag);
 };
 
+//追加ボタン押下後イベント発火
 document
   .getElementById("add-btn")
   .addEventListener("click", () => onClickAdd());
